0d4d8e5f5c23faa91e30a872864b4d21f42ab65d
reganthompson23 <reganthompson23>
2024-12-05 20:17:51 +1000

    feat: simplify dashboard to only show monthly stats

--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -132,14 +132,8 @@ function DemoLayout({ children }: { children: React.ReactNode }) {
 
 function DemoDashboard() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-8">
-      <div className="lg:col-span-2">
-        <ChatList />
-      </div>
-      <div className="space-y-4 sm:space-y-8">
-        <Stats />
-        <PricingRules rules={DEMO_BUSINESS.rules} />
-      </div>
+    <div className="max-w-3xl mx-auto">
+      <Stats />
     </div>
   );
 }
